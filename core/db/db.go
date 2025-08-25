ddc78b8534a70071fa13fcdf2ad7dd7223ac9b08
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-25 06:14:52 +0200

    chore: use datapath for db

--- core/db/db.go
+++ core/db/db.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"checklist/core/models"
+	"os"
+	"path/filepath"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -9,8 +11,28 @@ import (
 
 var DB *gorm.DB
 
+func GetAppDataPath() (string, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", nil
+	}
+	appDir := filepath.Join(configDir, "Checklist")
+	os.MkdirAll(appDir, os.ModePerm)
+
+	return filepath.Join(appDir, "todos.db"), nil
+}
+
 func InitDB() error {
-	database, err := gorm.Open(sqlite.Open("todos.db"), &gorm.Config{})
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return err
+	}
+	appDir := filepath.Join(configDir, "Checklist")
+	os.MkdirAll(appDir, os.ModePerm)
+
+	dbPath := filepath.Join(appDir, "todos.db")
+
+	database, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	if err != nil {
 		return err
 	}
