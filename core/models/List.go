9ad6f49ec41f12842653cb8f0121f7f29d7d8309
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-25 02:48:24 +0200

    models

--- core/models/List.go
+++ core/models/List.go
@@ -1,4 +1,10 @@
 package models
 
+import "time"
+
 type List struct {
+	ID        int32     `bson:"_id"`
+	Name      string    `bson:"name"`
+	CreatedAt time.Time `bson:"created"`
+	UpdatedAt time.Time `bson:"created"`
 }
--- core/models/Todo.go
+++ core/models/Todo.go
@@ -1,4 +1,11 @@
 package models
 
+import "time"
+
 type Todo struct {
+	ID          int32     `bson:"_id"`
+	Name        string    `bson:"name"`
+	Description string    `bson:"description"`
+	CreatedAt   time.Time `bson:"created"`
+	UpdatedAt   time.Time `bson:"created"`
 }
