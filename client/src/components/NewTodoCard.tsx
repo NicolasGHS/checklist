0b516d6054d12d422e4cd0a498d21a964e6778e1
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-26 19:33:08 +0200

    chore: description area in new task card

--- client/src/components/NewTodoCard.tsx
+++ client/src/components/NewTodoCard.tsx
@@ -2,21 +2,15 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "./ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "./ui/form";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Input } from "./ui/input";
 import { Card, CardContent } from "./ui/card";
 import { Checkbox } from "./ui/checkbox";
+import { Textarea } from "./ui/textarea";
 
 const formSchema = z.object({
   name: z.string().min(2).max(50),
+  description: z.string().min(0).max(255),
 });
 
 export const NewTodoCard = () => {
@@ -36,19 +30,36 @@ export const NewTodoCard = () => {
       <Card className="pt-4 mb-3">
         <CardContent>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
               <FormField
                 control={form.control}
                 name="name"
                 render={({ field }) => (
                   <FormItem>
-                    <FormControl className="flex items-center gap-3">
-                      <div>
+                    <FormControl>
+                      <div className="flex items-center gap-3">
                         <Checkbox disabled={true} />
                         <Input
                           placeholder="New Task"
                           {...field}
                           className="border-0 focus-visible:ring-0 focus-visible:outline-none shadow-none"
+                        />
+                      </div>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <div>
+                        <Textarea
+                          placeholder="Description"
+                          {...field}
+                          className="border-0 ml-7 focus-visible:ring-0 focus-visible:outline-none shadow-none"
                         />
                       </div>
                     </FormControl>
