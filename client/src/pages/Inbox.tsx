2b5c00da259965e2ec1afa6d44e72e43c21e7930
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-26 22:13:49 +0200

    refactor: unnused code in inbox

--- client/src/pages/Inbox.tsx
+++ client/src/pages/Inbox.tsx
@@ -1,17 +1,6 @@
-import React, { useEffect, useState } from "react";
-import {
-  AddTodo,
-  GetTodos,
-  ToggleTodo,
-  GetInboxTasks,
-} from "../../wailsjs/go/main/App";
-import { Task } from "../components/Task";
-import { Todo } from "../types/todo";
-import { NewTodoCard } from "../components/NewTodoCard";
-import { NewTaskButton } from "../components/NewTaskButton";
 import { Page } from "../components/Page";
 
-const Inbox: React.FC = () => {
+const Inbox = () => {
   return (
     <div>
       <Page title="Inbox" id={1} />
