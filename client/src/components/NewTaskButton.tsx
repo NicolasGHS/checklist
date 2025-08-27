38391ebae411b8bf9dd9d6dca30484fd5f6a377d
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-27 15:35:01 +0200

    chore: delete hover card

--- client/src/components/NewTaskButton.tsx
+++ client/src/components/NewTaskButton.tsx
@@ -1,6 +1,5 @@
 import { Button } from "./ui/button";
 import { Plus } from "lucide-react";
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
 
 type NewTaskProps = {
   onClick: () => void;
@@ -9,17 +8,12 @@ type NewTaskProps = {
 // TODO: make meta icon dynamic with windows
 export const NewTaskButton = ({ onClick }: NewTaskProps) => {
   return (
-    <HoverCard>
-      <HoverCardTrigger>
-        <Button
-          onClick={onClick}
-          size="icon"
-          className="fixed bottom-6 right-6 h-12 w-12 rounded-full shadow-lg"
-        >
-          <Plus className="h-6 w-6" />
-        </Button>
-      </HoverCardTrigger>
-      <HoverCardContent>⌘ + n</HoverCardContent>
-    </HoverCard>
+    <Button
+      onClick={onClick}
+      size="icon"
+      className="fixed bottom-6 right-6 h-12 w-12 rounded-full shadow-lg"
+    >
+      <Plus className="h-6 w-6" />
+    </Button>
   );
 };
