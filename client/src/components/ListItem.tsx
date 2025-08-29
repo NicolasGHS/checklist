79269d89b9c5fe50f576ff361c76fa2bb827ecc7
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-29 09:05:50 +0200

    chore: ellipsis on hover only

--- client/src/components/ListItem.tsx
+++ client/src/components/ListItem.tsx
@@ -31,6 +31,7 @@ type ListItemProps = {
 export const ListItem = ({ list }: ListItemProps) => {
   const location = useLocation();
   const [open, setOpen] = useState<boolean>(false);
+  const [isHovered, setIsHovered] = useState<boolean>(false);
 
   const handleDelete = async () => {
     await DeleteList(list.ID);
@@ -44,14 +45,18 @@ export const ListItem = ({ list }: ListItemProps) => {
         (location.pathname === "/" && list.Slug === "inbox")
       }
     >
-      <div className="flex items-center justify-between">
+      <div 
+        className="flex items-center justify-between"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <Link to={`list/${list.Slug}`} className="flex-1 px-2 py-1">
           <span>{list.Name}</span>
         </Link>
 
         <DropdownMenu>
           <DropdownMenuTrigger>
-            <Ellipsis className="w-4" />
+            <Ellipsis className={`w-4 transition-opacity duration-200 ${isHovered ? 'opacity-100' : 'opacity-0'}`} />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
