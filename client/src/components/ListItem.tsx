3d32ff20bceb29297648d8ba8f895fb710c6f17e
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-08-29 08:19:25 +0200

    chore: delete list dialog

--- client/src/components/ListItem.tsx
+++ client/src/components/ListItem.tsx
@@ -1,6 +1,27 @@
 import { Link, useLocation } from "react-router-dom";
 import { List } from "../types/list";
 import { SidebarMenuButton } from "./ui/sidebar";
+import { Ellipsis } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 type ListItemProps = {
   list: List;
@@ -8,6 +29,7 @@ type ListItemProps = {
 
 export const ListItem = ({ list }: ListItemProps) => {
   const location = useLocation();
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
     <SidebarMenuButton
@@ -17,9 +39,41 @@ export const ListItem = ({ list }: ListItemProps) => {
         (location.pathname === "/" && list.Slug === "inbox")
       }
     >
-      <Link to={`list/${list.Slug}`}>
-        <span>{list.Name}</span>
-      </Link>
+      <div className="flex items-center justify-between">
+        <Link to={`list/${list.Slug}`} className="flex-1 px-2 py-1">
+          <span>{list.Name}</span>
+        </Link>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Ellipsis />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => setOpen(true)}>
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <AlertDialog open={open} onOpenChange={setOpen}>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel onClick={() => setOpen(false)}>
+                Cancel
+              </AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </div>
     </SidebarMenuButton>
   );
 };
