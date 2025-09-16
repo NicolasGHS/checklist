4ae186d1155217d9ebd1fbe1e5f3166a7d0c48d7
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-09-16 13:00:39 +0200

    Task in Someday

--- client/src/pages/Someday.tsx
+++ client/src/pages/Someday.tsx
@@ -1,13 +1,63 @@
 import { useEffect, useState } from "react";
-import { GetArchivedTodos } from "../../wailsjs/go/main/App";
+import {
+  GetArchivedTodos,
+  ToggleTodo,
+  UpdateTodo,
+} from "../../wailsjs/go/main/App";
 import { models } from "../../wailsjs/go/models";
+import { Task } from "@/components/Task";
+import { TodoCard } from "@/components/TaskCard";
 
 export default function Someday() {
   const [todos, setTodos] = useState<models.Todo[]>([]);
+  const [openTodoId, setOpenTodoId] = useState<number>();
 
   const loadTodos = async () => {
     const result = await GetArchivedTodos();
     setTodos(result);
+  };
+
+  const toggleTodoCard = (id: number) => {
+    setOpenTodoId(id);
+  };
+
+  const updateTodo = async (
+    id: number,
+    name: string,
+    description: string,
+    list_id: number,
+    today: boolean,
+    deadline: string
+  ) => {
+    await UpdateTodo(id, name, description, list_id, today, deadline);
+  };
+
+  const handleToggle = async (id: number) => {
+    setTodos((prev) =>
+      prev.map((t) =>
+        t.ID === id
+          ? Object.assign(Object.create(Object.getPrototypeOf(t)), t, {
+              Completed: !t.Completed,
+            })
+          : t
+      )
+    );
+
+    try {
+      await ToggleTodo(id);
+    } catch (error) {
+      console.error(error);
+      setTodos((prev) =>
+        prev.map((t) =>
+          t.ID === id
+            ? Object.assign(Object.create(Object.getPrototypeOf(t)), t, {
+                Completed: !t.Completed,
+              })
+            : t
+        )
+      );
+    }
+    loadTodos();
   };
 
   useEffect(() => {
@@ -17,9 +67,21 @@ export default function Someday() {
   return (
     <div className="mt-10 flex flex-col min-h-full">
       <h1 className="text-3xl text-foreground font-bold mb-6">Someday</h1>
-      {todos.map((todo) => (
-        <p>{todo.Name}</p>
-      ))}
+      {todos
+        ?.filter((todo) => !todo.Completed)
+        .map((todo) => (
+          <li key={todo.ID} className="text-white mb-2">
+            {todo.ID !== openTodoId ? (
+              <Task
+                todo={todo}
+                onToggle={handleToggle}
+                openCard={toggleTodoCard}
+              />
+            ) : (
+              <TodoCard UpdateTodoFunction={updateTodo} Task={todo} />
+            )}
+          </li>
+        ))}
     </div>
   );
 }
