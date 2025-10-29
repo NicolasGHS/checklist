ecdda627b7df3fde773b48d5b51619738c5eff7f
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-10-29 15:46:44 +0100

    better vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,24 +1,24 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
-    }
+      "@": "/src",
+    },
   },
   optimizeDeps: {
-    include: ['react-markdown', 'remark-gfm']
+    include: ["react-markdown", "remark-gfm"],
   },
   build: {
     rollupOptions: {
-      external: []
+      external: [],
     },
     commonjsOptions: {
       include: [/node_modules/],
-      transformMixedEsModules: true
-    }
-  }
-})
+      transformMixedEsModules: true,
+    },
+  },
+});
